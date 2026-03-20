80897fd8f7859f4719263d0e9e367fdc5a910183
AiliVarshith <shivasaivarshith1902@gmail.com>
2026-03-20 06:33:22 +0000

    Add inittail monitor script

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#Created on 20/03/2026 in the morning
+
+#Author : Aili Varshith 
+
+#Note: This is my first basic project in learning DevOps and many more to go.
+
+
+#!/bin/bash
+
+echo "Health Monitor starting...."
+
